c22478610bb6012448bc485f01c7f72673045553
Dumindu <dumindu_km@yahoo.com>
2019-09-28 17:45:09 +0530

    create model using few features in LinearModel

--- house_price_predict.py
+++ house_price_predict.py
@@ -14,7 +14,7 @@ from sklearn.metrics import r2_score
 from matplotlib import rcParams
 
 # Pretty display for notebooks
-%matplotlib inline
+#%matplotlib inline
 
 
 #%%
@@ -38,6 +38,9 @@ data['PRICE'] = boston.target
 
 print(data.head())
 
+corr = data.corr()
+
+print("Co-relation to price",corr['PRICE'].sort_values(ascending=False))
 #%%
 
 #Let's visualize data
@@ -48,6 +51,9 @@ print(data.describe())
 Y = data['PRICE']
 X = data.drop('PRICE', axis = 1)
 
+# With the help of co-relation select only few columns for train
+min_features = ['RM','ZN','B','DIS','CHAS','AGE','CRIM','INDUS','PTRATIO','TAX']
+X_minimum_columns = X[min_features]
 
 #%%
 X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.30, random_state = 5)
@@ -56,33 +62,65 @@ print(X_test.shape)
 print(Y_train.shape)
 print(Y_test.shape)
 
-#%%
-lm = LinearRegression()
-lm.fit(X_train, Y_train)
+# Method for Train model using Linear Regression
+def TrainUsingLineearRegression(x,y):
+    lm = LinearRegression()
+    lm.fit(x, y)
+    return lm
 
+def PlotResultsUsingScattePlot(y_test, y_pred):
+    plt.scatter(y_test, y_pred)
+    plt.xlabel("Prices")
+    plt.ylabel("Predicted prices")
+    plt.title("Prices vs Predicted prices (Linear regression)")
+
+lm = TrainUsingLineearRegression(X_train, Y_train)
+# do prediction using Linear regression
 Y_pred = lm.predict(X_test)
 
-plt.scatter(Y_test, Y_pred)
-plt.xlabel("Prices")
-plt.ylabel("Predicted prices")
-plt.title("Prices vs Predicted prices")
+PlotResultsUsingScattePlot(Y_test, Y_pred)
+
+print('X_test')
+print(X_test.iloc[1])
 
 #%%
-mse = sklearn.metrics.mean_squared_error(Y_test, Y_pred)
-print(mse)
+def EvalateModel(y_test, y_pred):
+    mse = sklearn.metrics.mean_squared_error(y_test, y_pred)
+    print("Mean squarred error for full features",mse)
+    print("R2 score for full features",r2_score(y_test, y_pred))
+
+def DoPredictFromTestset(itemIndex, model, x_test, y_test):
+    results = model.predict([x_test.iloc[itemIndex]])
+    print("predicted price",results[0])
+    print("Actual price ",y_test.iloc[itemIndex])
+
+print('***** predict results using full features ******')
+
+EvalateModel(Y_test, Y_pred)
+DoPredictFromTestset(1,lm,X_test, Y_test)
+DoPredictFromTestset(2,lm,X_test, Y_test)
+DoPredictFromTestset(3,lm,X_test, Y_test)
+
+#%%
+print("Start training using few models")
+#Let's train model using few features
+# Let's train model using fewer features
+lm = TrainUsingLineearRegression(X_train[min_features], Y_train)
+# do prediction using Linear regression
+Y_pred = lm.predict(X_test[min_features])
+
+PlotResultsUsingScattePlot(Y_test, Y_pred)
+print('***** predict results using few features ******')
+
+EvalateModel(Y_test, Y_pred)
+DoPredictFromTestset(1,lm,X_test[min_features], Y_test)
+DoPredictFromTestset(2,lm,X_test[min_features], Y_test)
+DoPredictFromTestset(3,lm,X_test[min_features], Y_test)
+#%%
+
+
 
 #%%
 
-r2_score(Y_test, Y_pred)
-
-#%%
-
-index = 0
-results = lm.predict([X_test.loc[index]])
-print("predicted price",results[0])
-
-print("Actual price ",Y_test.loc[index])
-#%%
-X_test.keys
 
 #%%
